c4305b595572e9d89d7f944c9750b314579c3690
DanilYandulskyi <159798936+DanilYandulskyi@users.noreply.github.com>
2024-09-20 22:37:57 +0300

    Add files via upload

---
+++ Resource.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Movement))]
+public class Resource : MonoBehaviour
+{
+    private Movement _movement;
+    private Unit _followingUnit;
+    private bool _isFollowing = false;
+
+    private void Awake()
+    {
+        _movement = GetComponent<Movement>();   
+    }
+
+    private void Update()
+    {
+        if (_isFollowing)
+        {
+            transform.position = _followingUnit.transform.position - _followingUnit.MoveDirection.normalized;
+        }
+    }
+
+    public void Follow(Unit unit)
+    {
+        _followingUnit = unit;
+        _isFollowing = true;
+        _movement.SetSpeed(_followingUnit.MoveSpeed);
+    }
+}
